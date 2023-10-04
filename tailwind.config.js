98408885f40622b39ddaceff7bcfb050cf651caf
Shruti  Meshram <shruti.techfirm@gmail.com>
2023-10-05 02:18:08 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'cyan': 'rgb(66,190,165)',
+        '90vw':'90vw',
+    },
+  },
+  plugins: [],
+}
+}
